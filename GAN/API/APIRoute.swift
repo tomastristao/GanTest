e4c6fd4b1c3a3f929ad205b16064d866dd36d3f3
Tomas <48726972+tomastristao@users.noreply.github.com>
2021-04-26 16:19:13 +0100

    adding code

--- GAN/API/APIRoute.swift
+++ GAN/API/APIRoute.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum APIRoute {
+    case characters
+
+    var path: String {
+        let commonPath = "https://breakingbadapi.com/api/characters"
+
+        switch self {
+        case .characters:
+            return commonPath
+        }
+    }
+
+    var url: URL {
+        switch self {
+        case .characters:
+            return URL(string: self.path)!
+        }
+    }
+}
