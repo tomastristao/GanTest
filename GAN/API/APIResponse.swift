890569a52001b3dfa48cb15382f9d501aa921f21
Tomas <48726972+tomastristao@users.noreply.github.com>
2021-04-26 16:18:47 +0100

    adding code

--- GAN/API/APIResponse.swift
+++ GAN/API/APIResponse.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+typealias Plot = [BBCharacter]
+
+struct BBCharacter: Codable {
+    let charID: Int?
+    let name: String?
+    let birthday: String?
+    let occupation: [String]?
+    let img: String?
+    let status: Status?
+    let nickname: String?
+    let appearance: [Int]?
+    let portrayed: String?
+    let category: Category?
+    let betterCallSaulAppearance: [Int]?
+
+    enum CodingKeys: String, CodingKey {
+        case charID = "char_id"
+        case name, birthday, occupation, img, status, nickname, appearance, portrayed, category
+        case betterCallSaulAppearance = "better_call_saul_appearance"
+    }
+}
+
+enum Category: String, Codable {
+    case betterCallSaul = "Better Call Saul"
+    case breakingBad = "Breaking Bad"
+    case breakingBadBetterCallSaul = "Breaking Bad, Better Call Saul"
+}
+
+enum Status: String, Codable {
+    case alive = "Alive"
+    case deceased = "Deceased"
+    case presumedDead = "Presumed dead"
+    case unknown = "Unknown"
+}
