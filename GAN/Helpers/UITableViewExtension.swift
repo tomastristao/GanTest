392e0fd3b33180803c8f6985a102fdf18a7fac74
Tomas <48726972+tomastristao@users.noreply.github.com>
2021-04-26 16:20:59 +0100

    adding code

--- GAN/Helpers/UITableViewExtension.swift
+++ GAN/Helpers/UITableViewExtension.swift
@@ -5,4 +5,22 @@
 //  Created by Tom on 26/04/2021.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+    // Register cell from class reference
+    func register<T: UITableViewCell>(_: T.Type) {
+        let identifier = String(describing: T.self)
+        register(T.self, forCellReuseIdentifier: identifier)
+    }
+
+    // Dequeue cell from class reference
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
+        let identifier = String(describing: T.self)
+        guard let cell = dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? T else {
+            fatalError("Cell indentifier not found for: \(identifier)")
+        }
+
+        return cell
+    }
+}
